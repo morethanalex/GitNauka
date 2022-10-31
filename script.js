9e9b196c5f588141e87b5823f1d11723cd910fa7
morethanalex <116550119+morethanalex@users.noreply.github.com>
2022-10-31 12:26:34 +0100

    test

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-let title = "Finansowa forteca";
-let author = "Marcin Iwuc";
-let yearOfPub = 2020;
-let isNewerThan2000 = 1999 ;
-
-if(isNewerThan2000 >= 2000){
-    console.log("its younger");
-}
-else{
-    console.log("its older");
-}
-
+let book1 = "Finansowa forteca";
+let book2 = "Javascript for begginers";
+let book1yearOfPublic = 2020;
+let book2yearOfPublic = 2022;
+console.log(book1 + book2 );
