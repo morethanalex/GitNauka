5d028c78eb2fa2b808b8874c040c97ec829072e5
morethanalex <116550119+morethanalex@users.noreply.github.com>
2022-10-31 12:14:44 +0100

    proba

---
+++ script.js
@@ -0,0 +1,12 @@
+let title = "Finansowa forteca";
+let author = "Marcin Iwuc";
+let yearOfPub = 2020;
+let isNewerThan2000 = 1999 ;
+
+if(isNewerThan2000 >= 2000){
+    console.log("its younger");
+}
+else{
+    console.log("its older");
+}
+
